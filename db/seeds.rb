fe82300604437182bfca94030dcb4b3a4b77af27
Peter Suh <psuh22@gmail.com>
2019-10-27 13:29:43 +0800

    update to the code

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,3 +15,7 @@ drinks = JSON.parse(listOf)
 drinks['drinks'].each do |element|
   Ingredient.create(name: element['strIngredient1'])
 end
+
+Cocktail.create(name: "Martini", image_url: "https://res.cloudinary.com/peterfoo22/image/upload/v1571990001/t57zsbp7zzxtis8yijyj.jpg")
+Cocktail.create(name: "White Wine Spritzer", image_url: "https://res.cloudinary.com/peterfoo22/image/upload/v1571990001/t57zsbp7zzxtis8yijyj.jpg")
+Cocktail.create(name: "Mojito", image_url: "https://res.cloudinary.com/peterfoo22/image/upload/v1571989695/joqxgx6srmxxuqnucldj.jpg")
